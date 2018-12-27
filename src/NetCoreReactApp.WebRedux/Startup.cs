e4b4406368843db14e362757376a7241d2373312
russell walker <russ.h.walker@gmail.com>
2018-12-27 12:48:31 -0500

    Adding redux app

--- src/NetCoreReactApp.WebRedux/Startup.cs
+++ src/NetCoreReactApp.WebRedux/Startup.cs
@@ -6,7 +6,7 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
-namespace NetCoreReactApp.Web
+namespace NetCoreReactApp.WebRedux
 {
     public class Startup
     {
